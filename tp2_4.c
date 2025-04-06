47279e1b6adfdfd58089ad0a33c3d67984648323
colman nicolas <colman.nicolas.17@hotmail.com>
2025-04-06 20:33:29 -0300

    funcion listarPCs

--- tp2_4.c
+++ tp2_4.c
@@ -2,47 +2,60 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct compu
+{
+    int velocidad;        // Velocidad de procesamiento en GHz (valor entre 1 y 3)
+    int anio;             // Año de fabricación (valor entre 2015 y 2024)
+    int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
+    char *tipo_cpu;       // Tipo de procesador (apuntador a cadena de caracteres)
+} unaCompu;
+
 void listarPCs(struct compu pcs[],int cantidad){
-    
+    if (cantidad <= 0) return; 
+    for(int i=0; i<cantidad;i++){
+        printf("Computadora n°: %d", i+1);
+        printf("Velocidad: %d GHz\n", pcs[i].velocidad);
+        printf("Año: %d\n", pcs[i].anio);
+        printf("Núcleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("Tipo de CPU: %s\n", pcs[i].tipo_cpu);
+        printf("\n\n");
+    }
 }
 void mostrarMasVieja(struct compu pcs[],int cantidad){
-    
+    if (cantidad <= 0) return; 
+    int indice = 0;
+    int anioMin = pcs[0].anio;
+
+    for(int i = 1; i<cantidad; i++ ){
+        if (pcs[i].anio < anioMin)
+        {
+            anioMin = pcs[i].anio;
+            indice = i;
+        }            
+    }
+    printf("\n\nPC mas vieja");
+    listarPCs(&pcs[indice],1);
 }
-void listarPCs(struct compu pcs[],int cantidad){
+/*void listarPCs(struct compu pcs[],int cantidad){
     
-}
+}*/
 
 
 int main()
 {
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
-
     srand(time(NULL));
-    typedef struct compu
-    {
-        int velocidad;        // Velocidad de procesamiento en GHz (valor entre 1 y 3)
-        int anio;             // Año de fabricación (valor entre 2015 y 2024)
-        int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
-        char *tipo_cpu;       // Tipo de procesador (apuntador a cadena de caracteres)
-    } unaCompu;
+    unaCompu stockCompu[5];
 
-    unaCompu stockCompu[4];
     for (int i = 0; i < 5; i++)  //genero 5 computadoras
     {
         stockCompu[i].velocidad = 1 + rand() % 3;
         stockCompu[i].anio = 2015 + rand() % 10; // genero años entre 2015 y 2024 inclusives
         stockCompu[i].cantidad_nucleos = 1 + rand() % 8;
         stockCompu[i].tipo_cpu = *(tipos + rand() % 6); // genero un indice entre 0 y 5
-
     }
-
-
-    printf("Computadora n°: %d\n", i + 1);
-    printf("Velocidad: %d GHz\n", miComputadora.velocidad);
-    printf("Año: %d\n", miComputadora.anio);
-    printf("Núcleos: %d\n", miComputadora.cantidad_nucleos);
-    printf("Tipo de CPU: %s\n", miComputadora.tipo_cpu);
-    printf("\n\n");
+    
+    listarPCs(stockCompu, 5);
 
     return 0;
 }
